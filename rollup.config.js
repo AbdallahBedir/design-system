74b2729d23c1f416c37494102aa453a2c635a8e6
AbdallahBedir <abdallahahmed2025@gmail.com>
2022-05-28 19:28:57 +0200

    feat: add files key in package json file

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import typescript from 'rollup-plugin-typescript2';
+import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
 import commonjs from '@rollup/plugin-commonjs';
@@ -23,7 +24,11 @@ export default {
     },
   ],
   plugins: [
-    resolve(),
+    babel({
+      exclude: 'node_modules/**',
+      plugins: [['babel-plugin-styled-components', { fileName: false }]],
+    }),
+    resolve({ browser: true }),
     commonjs(),
     postcss(),
     typescript({
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,130 +1,129 @@
-import React, { ButtonHTMLAttributes } from "react";
+import React, { ButtonHTMLAttributes } from 'react';
 
-import styled from "styled-components";
+import styled from 'styled-components';
 
-import { Colors } from "colors";
+import { Colors } from '../../colors';
 
 const StyledButton = styled.button({
-  height: "2.5rem",
-  border: "1px solid transparent",
+  height: '2.5rem',
+  border: '1px solid transparent',
   borderRadius: 5,
-  cursor: "pointer",
-  fontSize: "0.75rem",
-  padding: "16px 25px",
-  outline: "transparent",
-  textTransform: "uppercase",
+  cursor: 'pointer',
+  fontSize: '0.75rem',
+  padding: '16px 25px',
+  outline: 'transparent',
+  textTransform: 'uppercase',
   backgroundColor: Colors.main_green,
-  color: "#fff",
+  color: '#fff',
   letterSpacing: 0.96,
-  lineHeight: "19px",
-  minWidth: "auto",
-  whiteSpace: "nowrap",
-  transition:
-    "color .2s ease, background-color .2s ease, border-color .2s ease",
+  lineHeight: '19px',
+  minWidth: 'auto',
+  whiteSpace: 'nowrap',
+  transition: 'color .2s ease, background-color .2s ease, border-color .2s ease',
 
   "&[data-btn-size='compact']": {
-    height: "2rem",
+    height: '2rem',
     fontSize: 10,
     fontWeight: 500,
     letterSpacing: 0.8,
-    lineHeight: "16px",
-    padding: "10px 24px",
+    lineHeight: '16px',
+    padding: '10px 24px',
   },
   "&[data-btn-size='small']": {
-    height: "1rem",
+    height: '1rem',
     fontSize: 9,
     fontWeight: 600,
     padding: 0,
     letterSpacing: 0,
   },
 
-  ":hover": {
-    backgroundColor: "#026B5B",
+  ':hover': {
+    backgroundColor: '#026B5B',
   },
-  ":active": {
-    backgroundColor: "#064946",
+  ':active': {
+    backgroundColor: '#064946',
   },
-  ":focus": {
-    borderColor: "#80B2FF",
+  ':focus': {
+    borderColor: '#80B2FF',
   },
-  ":disabled": {
+  ':disabled': {
     backgroundColor: Colors.green_disabled,
-    cursor: "not-allowed",
+    cursor: 'not-allowed',
     opacity: 0.25,
   },
 
   "&[data-btn-type='secondary']": {
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     color: Colors.main_green,
     border: `2px solid ${Colors.main_green}`,
 
-    ":hover": {
+    ':hover': {
       backgroundColor: Colors.main_green,
-      color: "#fff",
+      color: '#fff',
     },
-    ":active": {
-      color: "#fff",
-      backgroundColor: "#064946",
+    ':active': {
+      color: '#fff',
+      backgroundColor: '#064946',
     },
-    ":focus": {
-      borderColor: "#064946",
+    ':focus': {
+      borderColor: '#064946',
     },
   },
 
   "&[data-btn-type='secondaryGrey']": {
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     color: Colors.grey6,
     border: `2px solid ${Colors.grey6}`,
 
-    ":hover": {
+    ':hover': {
       backgroundColor: Colors.grey6,
-      color: "#fff",
+      color: '#fff',
     },
-    ":active": {
-      color: "#fff",
-      backgroundColor: "#064946",
+    ':active': {
+      color: '#fff',
+      backgroundColor: '#064946',
     },
-    ":focus": {
-      borderColor: "#064946",
+    ':focus': {
+      borderColor: '#064946',
     },
   },
 
   "&[data-btn-type='transparent']": {
-    backgroundColor: "transparent",
+    backgroundColor: 'transparent',
     color: Colors.main_green,
-    borderColor: "transparent",
+    borderColor: 'transparent',
 
-    ":hover": {
-      backgroundColor: "#EBEDF0",
+    ':hover': {
+      backgroundColor: '#EBEDF0',
     },
-    ":active": {
-      backgroundColor: "#D9DDE4",
+    ':active': {
+      backgroundColor: '#D9DDE4',
     },
-    ":focus": {
-      borderColor: "#80B2FF",
+    ':focus': {
+      borderColor: '#80B2FF',
     },
   },
   "&[data-btn-type='subtle']": {
-    color: "#00234BA8",
-    backgroundColor: "#EBEBED",
-    border: "1px solid transparent",
-    outline: "transparent",
-    ":hover": { backgroundColor: "#dcdde1" },
-    ":focus": {
-      borderColor: "#80B2FF",
+    color: '#00234BA8',
+    backgroundColor: '#EBEBED',
+    border: '1px solid transparent',
+    outline: 'transparent',
+    ':hover': { backgroundColor: '#dcdde1' },
+    ':focus': {
+      borderColor: '#80B2FF',
     },
   },
   "&[data-btn-type='lightGreen']": {
     backgroundColor: Colors.tertiary_green,
     color: Colors.main_green,
-    "&:hover": {
+    '&:hover': {
       backgroundColor: Colors.tertiary_green,
     },
   },
   "&[data-btn-type='error']": {
     backgroundColor: Colors.error,
     color: Colors.white,
-    "&:hover": {
+    '&:hover': {
       backgroundColor: Colors.error,
     },
   },
@@ -132,35 +131,35 @@ const StyledButton = styled.button({
 
 // Needed for safari because it has issues with flex inside buttons
 const StyledContentWrapper = styled.div({
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  height: "100%",
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
 });
 
 const StyledIconWrapper = styled.span({
-  marginRight: "8px",
-  display: "flex",
+  marginRight: '8px',
+  display: 'flex',
 });
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   btnType?:
-    | "default"
-    | "secondary"
-    | "transparent"
-    | "subtle"
-    | "secondaryGrey"
-    | "error"
-    | "lightGreen";
-  size?: "default" | "compact" | "small";
+    | 'default'
+    | 'secondary'
+    | 'transparent'
+    | 'subtle'
+    | 'secondaryGrey'
+    | 'error'
+    | 'lightGreen';
+  size?: 'default' | 'compact' | 'small';
   prefixIcon?: () => React.ReactNode;
   suffixIcon?: () => React.ReactNode;
   gtmId?: string;
 }
 
 const defaultProps: ButtonProps = {
-  btnType: "default",
-  size: "default",
+  btnType: 'default',
+  size: 'default',
   prefixIcon: undefined,
   suffixIcon: undefined,
 };
@@ -173,8 +172,8 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       disabled,
       prefixIcon,
       suffixIcon,
-      btnType = "default",
-      size = "default",
+      btnType = 'default',
+      size = 'default',
       children,
       id,
       gtmId,
@@ -185,7 +184,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     <StyledButton
       data-btn-type={btnType}
       data-btn-size={size}
-      type={type || "button"}
+      type={type || 'button'}
       disabled={disabled}
       onClick={(e) => {
         if (!disabled && onClick) {
@@ -208,7 +207,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   )
 );
 
-Button.displayName = "Button";
+Button.displayName = 'Button';
 Button.defaultProps = defaultProps;
 
 export { Button };
