ae72a9c63cae280abd5c0642675765e5b10385e0
AbdallahBedir <abdallahahmed2025@gmail.com>
2022-05-28 20:50:21 +0200

    feat: update prettierrc

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,7 @@
 module.exports = {
-  "printWidth": 80,
-  "trailingComma": "es5",
-  "tabWidth": 4,
-  "useTabs": false,
-  "endOfLine": "lf",
-  "semi": true,
-  "singleQuote": true,
-  "jsxSingleQuote": false,
-  "bracketSpacing": true,
-  "jsxBracketSameLine": false,
-  "arrowParens": "avoid"
+    printWidth: 100,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+    jsxSingleQuote: true,
 };
