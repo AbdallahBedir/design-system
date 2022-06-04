1cc885a06d2c1f187f357a725275171b508823d6
AbdallahBedir <abdallahahmed2025@gmail.com>
2022-06-04 13:18:34 +0200

    feat: add checkbox variations

--- src/components/Checkbox/Checkbox.stories.tsx
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -8,15 +8,18 @@ import { Checkbox } from './Checkbox';
 export default {
   title: 'Example/Checkbox',
   component: Checkbox,
+  args: {
+    children: 'Do you accept the conditions?',
+  },
 } as ComponentMeta<typeof Checkbox>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Checkbox> = (args) => <Checkbox {...args} />;
 
-export const Default = Template.bind({});
+export const Checked = Template.bind({});
 
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Default.args = {
+Checked.args = {
   checked: true,
 };
 
@@ -24,4 +27,24 @@ export const Unchecked = Template.bind({});
 
 Unchecked.args = {
   checked: false,
+};
+
+export const LabelStyle = Template.bind({});
+
+LabelStyle.args = {
+  labelStyle: {
+    fontSize: 14,
+    fontWeight: 300,
+    lineHeight: 1.5,
+    color: '#5a6487',
+  },
+};
+
+export const Small = Template.bind({});
+
+Small.args = {
+  checkboxSize: 18,
+  labelStyle: {
+    fontSize: 14,
+  },
 };
